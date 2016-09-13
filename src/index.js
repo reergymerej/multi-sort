323520ebed74d938572bc06a1b8de29d58b6e093
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-09-12 22:02:13 -0500

    handle reverse

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-function sort(collection) {
-	return collection.sort();
+function getBasicComparator(reverse = false) {
+	const direction = reverse
+		? -1
+		: 1;
+
+	return (a, b) => {
+		const aVal = a;
+		const bVal = b;
+
+		if (aVal < bVal) {
+			return -1 * direction;
+		} else if (bVal < aVal) {
+			return 1 * direction;
+		} else {
+			return 0;
+		}
+	};
 }
+
+function sort(collection, reverse = false) {
+	const comparator = getBasicComparator(reverse);
+	return collection.sort(comparator);
+}
+
 export default sort;
--- test/index.js
+++ test/index.js
@@ -8,4 +8,11 @@ describe('a simple sort', () => {
 		const expected = [1, 2, 3, 4, 5];
 		expect(actual).to.eql(expected);
 	});
+
+	it('should sort descending', () => {
+		const input = [1, 2, 3, 4, 5];
+		const actual = app(input, true);
+		const expected = [5, 4, 3, 2, 1];
+		expect(actual).to.eql(expected);
+	});
 });
