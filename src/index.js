d57db5e2ef97f1324005a002734adb66c89b4dda
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-09-13 22:59:01 -0500

    change indentation

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
 function getBasicComparator(reverse = false) {
-	const direction = reverse
-		? -1
-		: 1;
+  const direction = reverse
+    ? -1
+    : 1;
 
-	return (a, b) => {
-		const aVal = a;
-		const bVal = b;
+  return (a, b) => {
+    const aVal = a;
+    const bVal = b;
 
-		if (aVal < bVal) {
-			return -1 * direction;
-		} else if (bVal < aVal) {
-			return 1 * direction;
-		} else {
-			return 0;
-		}
-	};
+    if (aVal < bVal) {
+      return -1 * direction;
+    } else if (bVal < aVal) {
+      return 1 * direction;
+    } else {
+      return 0;
+    }
+  };
 }
 
 
 function basicSort(collection, reverse = false) {
-	const comparator = getBasicComparator(reverse);
-	return collection.sort(comparator);
+  const comparator = getBasicComparator(reverse);
+  return collection.sort(comparator);
 }
 
 function sortStrategy(...args) {
-	const [collection, options] = args;
-	if (typeof options === 'boolean' || typeof options === 'undefined') {
-		const reverse = options;
-		return basicSort(collection, reverse);
-	}	
+  const [collection, options] = args;
+  const type = typeof options;
+
+  switch (type) {
+    case 'undefined':
+    case 'boolean':
+      const reverse = options;
+      return basicSort(collection, reverse);
+      break;
+    case 'string':
+      return [1,2,3];
+    default:
+  }
 }
 
 export default sortStrategy;
--- test/index.js
+++ test/index.js
@@ -2,43 +2,43 @@ import { expect } from 'chai';
 import app from '../src';
 
 describe('a simple sort', () => {
-	it('should sort ascending', () => {
-		const input = [5, 4, 3, 2, 1];
-		const actual = app(input);
-		const expected = [1, 2, 3, 4, 5];
-		expect(actual).to.eql(expected);
-	});
+  it('should sort ascending', () => {
+    const input = [5, 4, 3, 2, 1];
+    const actual = app(input);
+    const expected = [1, 2, 3, 4, 5];
+    expect(actual).to.eql(expected);
+  });
 
-	it('should sort descending', () => {
-		const input = [1, 2, 3, 4, 5];
-		const actual = app(input, true);
-		const expected = [5, 4, 3, 2, 1];
-		expect(actual).to.eql(expected);
-	});
+  it('should sort descending', () => {
+    const input = [1, 2, 3, 4, 5];
+    const actual = app(input, true);
+    const expected = [5, 4, 3, 2, 1];
+    expect(actual).to.eql(expected);
+  });
 });
 
-describe('sorting objects by a single property', () => {
-	it('should sort ascending', () => {
-		const input = [
-			{ name: 'c' },
-			{ name: 'b' },
-			{ name: 'a' },
-		];
-		const field = 'name';
-		const actual = app(input, field);
-		const expected = [
-			{ name: 'a' },
-			{ name: 'b' },
-			{ name: 'c' },
-		];
-		expect(actual).to.eql(expected);
-	});
+describe.only('sorting objects by a single property', () => {
+  it('should sort ascending', () => {
+    const input = [
+      { name: 'c' },
+      { name: 'b' },
+      { name: 'a' },
+    ];
+    const field = 'name';
+    const actual = app(input, field);
+    const expected = [
+      { name: 'a' },
+      { name: 'b' },
+      { name: 'c' },
+    ];
+    expect(actual).to.eql(expected);
+  });
 
-	
-	xit('should sort descending', () => {
-		const input = [1, 2, 3, 4, 5];
-		const actual = app(input, true);
-		const expected = [5, 4, 3, 2, 1];
-		expect(actual).to.eql(expected);
-	});
+
+  xit('should sort descending', () => {
+    const input = [1, 2, 3, 4, 5];
+    const actual = app(input, true);
+    const expected = [5, 4, 3, 2, 1];
+    expect(actual).to.eql(expected);
+  });
 });
